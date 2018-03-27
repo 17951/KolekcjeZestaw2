a9d584fa54c1534a157e4dd4dcc11da039609df9
Student <Student@193.107.34.219>
2018-03-27 11:01:19 +0200

    v1

---
+++ Zad1/src/OdwrocenieMapy.java
@@ -0,0 +1,13 @@
+//Odwrócenie kluczy i wartoœci
+//Napisz program, który z kolekcji HashMap<Integer,Integer> stworzy now¹ mapê, gdzie klucze i wartoœci s¹ zamienione miejscami.
+
+public class OdwrocenieMapy {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	
+}
