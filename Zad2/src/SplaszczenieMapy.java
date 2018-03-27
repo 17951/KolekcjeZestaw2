2866e1017a38e320aa53dcfc1b2138a8d2334356
Student <Student@193.107.34.219>
2018-03-27 11:21:54 +0200

    podpunkt a

---
+++ Zad2/src/SplaszczenieMapy.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//Sp³aszczenie mapy
+//a. Z wszystkich kluczy i wartoœci kolekcji HashMap<Integer,Integer> utwórz kolekcjê ArrayList<Integer>. 
+//b. Z wszystkich kluczy i wartoœci kolekcji HashMap<Integer,HashMap<Integer,Integer>> utwórz kolekcjê ArrayList<Integer>. 
+//Np dla mapy [[3,[[5,2],[7,9]], [7,[[4,4],[2,11]]] wynikiem bêdzie [3,5,2,7,9,7,4,4,2,11] (niekoniecznie w takiej kolejnoœci).
+
+public class SplaszczenieMapy {
+	public static void main(String[] args) {
+
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(3, 9);
+		map.put(2, 4);
+		map.put(4, 16);
+		map.put(6, 36);
+		map.put(5, 25);
+
+		List<Integer> list = new ArrayList<>();
+
+		for (Integer k : map.keySet()) {
+			list.add(k);
+			list.add(map.get(k));
+		}
+
+		System.out.println(list);
+		
+//		Map<Integer,HashMap<Integer,Integer>> mapa1= new HashMap<Integer,HashMap<Integer,Integer>>();
+		
+	}
+}
