cd36a7686f8045ba7f94f6d0ecae503534db8368
Student <Student@193.107.34.219>
2018-03-27 11:20:38 +0200

    tylko podpunkt a

--- Zad1/src/OdwrocenieMapy.java
+++ Zad1/src/OdwrocenieMapy.java
@@ -8,7 +8,7 @@ public class OdwrocenieMapy {
 
 	public static void main(String[] args) {
 
-		Map<Integer, Integer> map = new HashMap();
+		Map<Integer, Integer> map = new HashMap<>();
 		map.put(1, 3);
 		map.put(3, 9);
 		map.put(2, 4);
