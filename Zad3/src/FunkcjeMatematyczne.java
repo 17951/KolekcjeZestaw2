c8aa3f714f2d3dcbb4e91c763b84f09468b4493f
Student <Student@193.107.34.219>
2018-03-27 11:23:36 +0200

    v1

---
+++ Zad3/src/FunkcjeMatematyczne.java
@@ -0,0 +1,9 @@
+//Interpretujemy mapê typu HashMap<Integer,Integer> jako funkcjê (w sensie matematycznym). Jeœli f(x)=y, to w mapie bêdzie para [x,y].
+//a. Napisz statyczn¹ metodê, która dla podanych dwóch funkcji zwróci ich z³o¿enie (now¹ mapê). 
+//b. Napisz statyczn¹ metodê zwracaj¹c¹ funkcjê odwrotn¹ do podanej jako parametr; jeœli funkcja odwrotna nie istnieje, wyrzucony zostaje wyj¹tek BrakOdwrotnej.
+
+public class FunkcjeMatematyczne {
+	public static void main(String[] args) {
+
+	}
+}
